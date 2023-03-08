dc28d579243b257c1ecd428fb0360ff1d92a01f0
02Gqbriel <eglig@bzz.ch>
2023-03-08 22:43:13 +0100

    advanced chunksplitting

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import viteCompression from 'vite-plugin-compression';
 import { imagetools } from 'vite-imagetools';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		svelte({}),
 		imagetools({}),
+		chunkSplitPlugin({ strategy: 'unbundle' }),
 		viteCompression({ algorithm: 'brotliCompress' }),
 		viteCompression({ algorithm: 'gzip' }),
 	],
