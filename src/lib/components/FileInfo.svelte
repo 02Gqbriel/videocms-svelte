4ac8f11a8cf858b5e0b45656bb432418f7330f3f
Gabriel Egli <eglig@bzz.ch>
2023-05-27 20:13:05 +0200

    test push

--- src/lib/components/FileInfo.svelte
+++ src/lib/components/FileInfo.svelte
@@ -6,5 +6,5 @@
 	on:click|stopPropagation={closeInfo}
 	class="absolute right-0 mx-2 rounded-lg border-2 border-neutral-800 border-opacity-50  bg-neutral-900/80 backdrop-blur-sm"
 >
-	<div class="p-2">king</div>
+	<div class="p-2">king 2</div>
 </button>
